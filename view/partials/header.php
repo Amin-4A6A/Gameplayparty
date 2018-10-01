f00055d1b09141649fd7492748f73e9f5e5fbce7
Amin-4A6A <510074@edu.rocmn.nl>
2018-10-01 12:43:21 +0200

    over ons weg

--- view/partials/header.php
+++ view/partials/header.php
@@ -13,7 +13,7 @@
 </head>
 <body>
 <nav class="nav-top navbar navbar-expand-md fixed-top" style="background: #A3CB38 !important;">
-<img src="../gpp/gpp.svg" width="350" height="60" alt="">
+<img src="../view/images/gpp.svg" width="350" height="60" alt="">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -21,9 +21,6 @@
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Over ons</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/contactController/contact">Contact</a>
