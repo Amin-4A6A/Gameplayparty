1f637ef6da0d04b8f3e540001020737be493bac5
Wesley-4A6B <483509@edu.rocmn.nl>
2018-10-01 12:53:29 +0200

    Fixed home

--- view/home.php
+++ view/home.php
@@ -4,8 +4,8 @@
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
       <div class="col-md-5 p-lg-5 mx-auto my-5">
         <h1 class="display-4 font-weight-normal">Welkom op Gameplayparty</h1>
-        <p class="lead font-weight-normal">Hier kunt u een reservering plaatsen voor een leuke feest of het uiptproberen van u volgende game</p>
-        <a class="btn btn-outline-secondary" href="#">catalogus</a>
+        <p class="lead font-weight-normal">Hier kunt u een reservering plaatsen voor een leuke feest of het uitproberen van uw volgende game!</p>
+        <a class="btn btn-outline-secondary" href="#">Catalogus</a>
       </div>
       <div class="product-device shadow-sm d-none d-md-block"></div>
       <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
