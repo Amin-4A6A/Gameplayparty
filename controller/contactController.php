7e267bd571cafb029ed88fc2f6d754f4ac887dc2
Brandon <brandon.louw@hotmail.nl>
2018-10-10 15:34:51 +0200

    foto home toegevoegd

--- controller/contactController.php
+++ controller/contactController.php
@@ -21,10 +21,27 @@ class contactController
 		 	$email = $_POST['email'];
 		 	$bericht = $_POST['bericht'];
 
+		 	// if 
+
 		 	$email= mail("brandon.louwaars@gmail.com", "Contact", "Naam: " . $naam . "<br>" . "email: " . $email . "<br><br>" . "Bericht: " . $bericht);
 		 } else {
+		 	
 		 	$this->contact();
 		 }
 	}
 		
 }
+
+// $naam = isset($_POST["naam"]);
+    //         // check name only contains letters and whitespace
+    //         if (!preg_match("/^[a-zA-Z ]*$/",$naam))
+    //         {
+    //             throw new dataException('alleen letters en witruimtes worden geaccepteerd');
+    //         }   
+    //         else 
+    //         {
+    //             if(str_word_count($naam)!=2)
+    //             {
+    //                  throw new dataException('naam moet worden in gevuld achternaam is optioneel');
+    //             }
+    //         }
--- view/home.php
+++ view/home.php
@@ -4,6 +4,7 @@
 
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
       <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <img src="../view/images/homePic.jpg" style="height:500px; width: 900px;" class="">
         <h1 class="display-4 font-weight-normal">Welkom op Gameplayparty</h1>
         <p class="lead font-weight-normal">Hier kunt u een reservering plaatsen voor een leuke feest of het uitproberen van uw volgende game!</p>
         <a class="btn btn-outline-secondary" href="/Gameplayparty/catalogusController/showCatalogus">Catalogus</a>
@@ -16,15 +17,12 @@
     <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading">Over Ons. <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">
-               
-            </p>
+            <p class="lead"></p>
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" src="../images/bioszaal.jpg/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1663605ec68%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1663605ec68%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.125%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
           </div>
         </div>
-    <hr class="featurette-divider">
-   
+    <hr class="featurette-divider">   
 
 <?php include('partials/footer.php'); ?>
