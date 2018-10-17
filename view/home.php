0b0939c3e372d4bf673ad14af6fd2feae0b5a0a3
Brandon <brandon.louw@hotmail.nl>
2018-10-17 13:17:43 +0200

    wijzeging overons

--- view/home.php
+++ view/home.php
@@ -18,7 +18,9 @@
         <p class="lead font-weight-normal">Hier kunt u een reservering plaatsen voor een leuke feest of het uitproberen van uw volgende game!</p>
         <a class="btn btn-outline-secondary" href="#">Coming soon</a>
       </div>
+      <img src="../view/images/homePic.jpg" style="width: 900px; height: 700px;">
   </div>
+
 </div>
 
     <div class="crstyle container">
@@ -26,10 +28,10 @@
     <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading">Over Ons. <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead"></p>
+            <p class="lead"> Zoek jij een leuke plek om je verjaardag te vieren of een game te testen? We kunnen je de hand schudden, dat deden wij ook.  Bioscopen kunnen nooit worden afgehuurd door particulieren, en als het wel kan gaat dat vaak heel moeilijk. Dat kan anders, beter. We hebben gezocht en gezocht en gezocht en kwamen er achter dat het wél kan: bioscopen afhuren voor jouw feestje kan echt!</p>
           </div>
           <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="../images/bioszaal.jpg/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1663605ec68%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1663605ec68%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.125%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+            <img class="featurette-image img-fluid mx-auto" src="../view/images/console.jpg" alt="500x500" style="width: 400px; height: 250px;" data-holder-rendered="true">
           </div>
         </div>
     <hr class="featurette-divider">   
