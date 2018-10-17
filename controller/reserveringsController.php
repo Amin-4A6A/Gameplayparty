7d87a245ed816d4c5db60b6787edfc2a881156e6
Amin-4A6A <510074@edu.rocmn.nl>
2018-10-17 09:49:20 +0200

    resevering

--- controller/reserveringsController.php
+++ controller/reserveringsController.php
@@ -26,15 +26,16 @@ class reserveringsController
             $postcode = $_POST['postcode'];
             $stad = $_POST['stad'];
             $provincie = $_POST['provincie'];
-			$klanten = $this->reserveringsModel->createReservering($voornaam, $achternaam , $telefoonnummer, $straatnaam, $huisnummer, $toevoeging, $postcode, $stad, $provincie);
+			$bioscopen = $this->reserveringsModel->createReservering($voornaam, $achternaam , $telefoonnummer, $straatnaam, $huisnummer, $toevoeging, $postcode, $stad, $provincie, $reservering_date, $begin_tijd , $eind_tijd, $aantal_personnen);
 			$reservering_date = $_POST['reservering_date'];
 			$begin_tijd = $_POST['begin_tijd'];
 			$eind_tijd = $_POST['eind_tijd'];
 			$aantal_personnen = $_POST['aantal_personnen'];
-			$klanten = $this->reserveringsModel->createReservering($reservering_date, $begin_tijd , $eind_tijd, $aantal_personnen);
-			$tijden = $this->reserveringsModel->createReservering();
-			header("Location: {$_SERVER['HTTP_REFERER']}");
-			exit;
+			$klanten = $this->reserveringsModel->createReservering();
+			// $tijden = $this->reserveringsModel->createReservering();
+			// header("Location: {$_SERVER['HTTP_REFERER']}");
+			// exit;
+			include('view/reserveringsform.php');
 		} else {
 			require_once('view/reserveringsform.php');
 		}
--- view/reserveringsform.php
+++ view/reserveringsform.php
@@ -1,4 +1,5 @@
 <?php include('partials/header.php'); ?>
+<?php include(''); ?>
 <form method="post" action="../reserveringsController/collectreservering/">
         <h1> Bioscoop reserveren </h1>
         <div class="form-group">
@@ -42,13 +43,13 @@
             <input type="number" class="form-control" name="aantal_personnen" required>
         </div>
         <label for="">Selecteer een datum</label>
-                    <select class="form-control mb-5" <?= $disabled[0]; ?>>
-                    <?= $disabled[1]; ?>
-                    <?php foreach ($tijden as $tijd): ?>
-                            <option value="1" class="form-control"><?= date("D H:i", strtotime($tijd['begintijd'])) ?> - <?= date("H:i", strtotime($tijd['eindtijd'])) ?></option>
+                    <select class="form-control mb-5">
+                    <?php foreach ($bioscopen as $bioscoop): ?>
+                    <?php var_dump($bioscoop);?>
+                            <option value="1" class="form-control"><?= date("D H:i", strtotime($bioscoop['begintijd'])) ?> - <?= date("H:i", strtotime($bioscoop['eindtijd'])) ?></option>
                         <?php endforeach; ?>
                     </select>
-                            <?php var_dump($tijd['begintijd']); ?>
+                           
                     <!--  -->
         <!-- <div class="form-group">
             <label for="provincie">datum&#42;</label>
