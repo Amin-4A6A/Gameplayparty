04fd082c33d999775faa88cecc2434272e902d0d
Brandon <brandon.louw@hotmail.nl>
2018-10-17 13:58:57 +0200

    over ons gewijzigd

--- view/home.php
+++ view/home.php
@@ -12,13 +12,13 @@
       </div>
     </div>
   </div> -->
-  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-      <div class="col-md-5 p-lg-5 mx-auto my-5 image_top">
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 bg-light">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
         <h1 class="display-4 font-weight-normal">Welkom op Gameplayparty</h1>
         <p class="lead font-weight-normal">Hier kunt u een reservering plaatsen voor een leuke feest of het uitproberen van uw volgende game!</p>
         <a class="btn btn-outline-secondary" href="#">Coming soon</a>
       </div>
-      <img src="../view/images/homePic.jpg" style="width: 900px; height: 700px;">
+      <img src="../view/images/homePic.jpg" style="width: 1000px; height: 700px;">
   </div>
 
 </div>
@@ -27,7 +27,7 @@
     <hr class="featurette-divider">
     <div class="row featurette">
           <div class="col-md-7">
-            <h2 class="featurette-heading">Over Ons. <span class="text-muted">It'll blow your mind.</span></h2>
+            <h2 class="featurette-heading">Over Ons</h2>
             <p class="lead"> Zoek jij een leuke plek om je verjaardag te vieren of een game te testen? We kunnen je de hand schudden, dat deden wij ook.  Bioscopen kunnen nooit worden afgehuurd door particulieren, en als het wel kan gaat dat vaak heel moeilijk. Dat kan anders, beter. We hebben gezocht en gezocht en gezocht en kwamen er achter dat het wél kan: bioscopen afhuren voor jouw feestje kan echt!</p>
           </div>
           <div class="col-md-5">
